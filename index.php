cd677cacbcc89d2cec8f9a5241ffb4ffc83a9f95
mambax7 <mambax7@gmail.com>
2020-07-11 22:47:36 -0400

    initial
    Signed-off-by: mambax7 <mambax7@gmail.com>

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+include("../../mainfile.php");
+$xoopsOption['template_main'] = "paintball.html";
+include(XOOPS_ROOT_PATH."/header.php");
+include("header.php");
+
+$xoopsOption['show_rblock'] = 1;
+ ?>
+<html>
+<body>
+<p>
+<a href="http://www.miniclip.com/games/en/">Stress Relief Paintball by Miniclip.com</a>
+<p>
+<a href="http://shadowsbastion.com/">Ported to XOOPS by Shadow's Bastion</a>
+</body>
+</html>
+
+
+<?php
+include(XOOPS_ROOT_PATH."/footer.php");
+?>
---
+++ xoops_version.php
@@ -0,0 +1,17 @@
+<?php
+$modversion['name'] = "Stress_Relief_Paintball";
+$modversion['version'] = 1.00;
+$modversion['description'] = "Flash games hosted on your site.";
+$modversion['author'] = "Ported To Xoops By Shadow (<a href='http://shadowsbastion.com/'>Shadow's Bastion</a>)";
+$modversion['credits'] = "Stress Relief paitball by Miniclip(<a href='http://www.miniclip.com/games/en/'>Miniclip.com</a>)";
+$modversion['help'] = "";
+$modversion['license'] = "GPL see LICENSE";
+$modversion['official'] = 0;
+$modversion['image'] = "images/paintbigicon.jpg";
+$modversion['dirname'] = "paintball";
+$modversion['hasAdmin'] = 0;
+$modversion['adminpath'] = "";
+$modversion['hasMain'] = 1;
+$modversion['templates'][1]['file'] = "paintball.html";
+$modversion['templates'][1]['description'] = "The template file of index";
+?>
